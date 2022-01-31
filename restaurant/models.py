4ab28154856428b1ee34b77a621aef8e676ec81b
pythonguru <pythonguru101@gmail.com>
2022-01-31 09:10:13 -0800

    added model for restaurant and menu

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from core.base_model import BaseModel
 # Create your models here.
+
+
+class Restaurant(BaseModel):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Menu(BaseModel):
+    restaurant = models.ForeignKey(Restaurant, related_name="menus", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.id)
